8aefd52c5f58995e385467ff2da44390ac226caa
Kyle Lam <kyle.lam@mail.utoronto.ca>
2024-09-29 23:31:18 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,108 @@
+import java.io.*;
+
 public class Driver {
     public static void main(String [] args) {
+        //constructor & evaluation
         Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
+        System.out.println("empty polynomial p(3): expect 0, got: " + p.evaluate(3));
+        //expect 0
+
         double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
+        int [] e1 = {0,6,2,6};
+        Polynomial p1 = new Polynomial(c1,e1);
+        System.out.println("polynomial coefficients and exponents p1(1): expect 11, got: " + p1.evaluate(1));
+        //expect 11
+
         double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        int [] e2 = {1,2,0,2,4};
+        Polynomial p2 = new Polynomial(c2,e2);
+        System.out.println("polynomial coefficients and exponents p2(2): expect -152, got: " + p2.evaluate(2));
+        //expect -152
+
+        double [] c3 = {3,0,5};
+        int [] e3 = {2,3,1};
+        Polynomial p3 = new Polynomial(c3,e3);
+        System.out.println("polynomial coefficients and exponents p3(1): expect 8, got: " + p3.evaluate(1));
+        //expect 8
+
+        double [] c4 = {-1,4,0};
+        int [] e4 = {1,3,0};
+        Polynomial p4 = new Polynomial(c4,e4);
+        System.out.println("polynomial coefficients and exponents p4(2): expect 30, got: " + p4.evaluate(2));
+        //expect 30
+
+        //edge cases - 0 coefficients
+        double [] c5 = {0,0,0};
+        int [] e5 = {0,1,2};
+        Polynomial p5 = new Polynomial(c5,e5);
+        System.out.println("polynomial coefficients and exponents p5(3): expect 0, got: " + p5.evaluate(3));
+        //big values and big exponents
+        double [] c6 = {1000,-5000,2000};
+        int [] e6 = {100,50,10};
+        Polynomial p6 = new Polynomial(c6,e6);
+        System.out.println("polynomial coefficients and exponents p6(2): expect ~1.2676506E33, got: " + p6.evaluate(2));
+
+
+        double [] c7 = {6,-2,5};
+        int [] e7 = {0,1,3};
+        Polynomial p7 = new Polynomial(c7,e7);
+
+        double [] c8 = {3,0,4};
+        int [] e8 = {3,2,8};
+        Polynomial p8 = new Polynomial(c8,e8);
+
+
+        //addition
         Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        System.out.println("addition s=p1+p2, then s(0.1): expect ~5.98, got: " + s.evaluate(0.1));
+
+        Polynomial s2 = p3.add(p4);
+        System.out.println("addition s2=p3+p4, then s2(0.1): expect ~0.434, got: " + s2.evaluate(0.1));
+
+        Polynomial s3 = p7.add(p8);
+        System.out.print("\naddition s3=p7+p8: expect poly values: 6,-2,8,4, got: ");
+        for (int i = 0; i < s3.poly.length; i++) {
+            System.out.print(s3.poly[i]+",");
+        }
+        System.out.print("\naddition s3=p7+p8: expect expo values: 0,1,3,8, got:");
+        for (int i = 0; i < s3.expo.length; i++) {
+            System.out.print(s3.expo[i]+",");
+        }
+
+        //multiplication
+        Polynomial m = p1.multiply(p2);
+        System.out.println("\n\nmultiplication m=p1*p2, then m(2): expect -49552, got: " + m.evaluate(2));
+
+        Polynomial m2 = p3.multiply(p4);
+        System.out.println("multiplication m2=p3*p4, then m2(2): expect 660, got: " + m2.evaluate(2));
+
+        //root
+        System.out.println("\nroot test 1: expect '1 is a root of s', got: ");
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        System.out.println("\nroot test 2: expect '1 is not a root of s2', got: ");
+        if(s2.hasRoot(1))
+            System.out.println("1 is a root of s2");
+        else
+            System.out.println("1 is not a root of s2");
+
+        //file reading and saving
+        System.out.println("\nfile reading and saving, reading from 'polynomial.txt', outputs s2 to 'result.txt'");
+        try {
+            Polynomial filePoly = new Polynomial(new File("polynomial.txt"));
+            System.out.println("polynomial from file filePoly(2): expect 30, got: " + filePoly.evaluate(2));
+
+            s2.saveToFile("result.txt");
+            System.out.println("Saved polynomial s2 to 'result.txt', expect '4x+3x2+4x3' in file");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,46 +1,248 @@
-//CSCB07 Lab 1 - Kyle Lam (1008177090), September 22, 2024
+//CSCB07 Lab 2 - Kyle Lam (1008177090), September 29, 2024
+import java.io.*;
+import java.util.Scanner;
 
 public class Polynomial {
-    //field to represent coefficients of the polynomial using an array of double
+    //fields to represent coefficients of the polynomial using an array of double and exponents using an array of int
     double[] poly;
+    int[] expo;
 
-    //initialize Polynomial without args - sets corresponding array to [0]
+    //initialize Polynomial without args - sets corresponding poly and expo arrays to [0]
     public Polynomial() {
         poly = new double[]{0};
+        expo = new int[]{0};
     }
-    //initialize Polynomial with args - sets corresponding array according to the inputs array
-    public Polynomial(double[] inputs) {
+    //initialize Polynomial with args - sets corresponding poly and expo arrays according to inputs arrays
+    public Polynomial(double[] inputs, int[] expoInputs) {
         poly = inputs;
+        expo = expoInputs;
     }
-    //add two polynomials - finds max polynomial length, returns new Polynomial with sum of coefficients at each degree
-    public Polynomial add(Polynomial addPoly) {
-        int maxPolyLen = Math.max(this.poly.length, addPoly.poly.length);
-        double[] newPoly = new double[maxPolyLen];
-
-        for (int i = 0; i < maxPolyLen; i++) {
-            double coeff;
-            double coeffAdd;
-
-            if (i < this.poly.length) coeff = this.poly[i];
-            else coeff = 0;
-
-            if (i < addPoly.poly.length) coeffAdd = addPoly.poly[i];
-            else coeffAdd = 0;
-
-            newPoly[i] = coeff + coeffAdd;
+    //initialize Polynomial with input file - reads file, splits into terms and parts of terms, sets poly and expo arrays accordingly
+    public Polynomial(File inputFile) throws FileNotFoundException {
+        Scanner file = new Scanner(inputFile);
+        String content = file.nextLine();
+        //converts "-" into "+-" for easier splitting, splits with "\\+" to ensure it is the "+" character and not regex
+        String modContent = content.replace("-", "+-");
+        //removes redundant "+" from first term if it was negative but got its "-" replaced with "+-"
+        if (modContent.startsWith("+")) {
+            modContent = modContent.substring(1);
         }
-        return new Polynomial(newPoly);
+        String[] terms = modContent.split("\\+");
+        //number of terms after split is the size of polynomial
+        poly = new double[terms.length];
+        expo = new int[terms.length];
+        //iterates the terms array, if a term has "x" then split the term one more time, parse into poly and expo arrays accordingly
+        for (int i = 0; i < terms.length; i++) {
+            if (terms[i].contains("x")) {
+                String[] termSplit = terms[i].split("x");
+
+                if (termSplit[0].equals("")) {
+                    poly[i] = 1;
+                }
+                else if (termSplit[0].equals("-")) {
+                    poly[i] = -1;
+                }
+                else {
+                    poly[i] = Double.parseDouble(termSplit[0]);
+                }
+
+                if (termSplit.length > 1 && !termSplit[1].equals("")) {
+                    expo[i] = Integer.parseInt(termSplit[1]);
+                }
+                else {
+                    expo[i] = 1;
+                }
+            }
+            else {
+                poly[i] = Double.parseDouble(terms[i]);
+                expo[i] = 0;
+            }
+        }
     }
-    //evaluate polynomial - returns the result of polynomial according to argument value x and increasing exponent
+    //helper - sort polynomial to ascending order in exponent degree
+    public void sortExpo() {
+        for (int i = 0; i < expo.length - 1; i++) {
+            for (int j = 0; j < expo.length - i - 1; j++) {
+                if (expo[j] > expo[j+1]) {
+                    int tempExpo = expo[j+1];
+                    double tempPoly = poly[j+1];
+
+                    expo[j+1] = expo[j];
+                    poly[j+1] = poly[j];
+
+                    expo[j] = tempExpo;
+                    poly[j] = tempPoly;
+                }
+            }
+        }
+    }
+    //add two polynomials - returns new Polynomial with sum of coefficients at each degree
+    public Polynomial add(Polynomial addPoly) {
+        //sort polynomials for easier addition
+        this.sortExpo();
+        addPoly.sortExpo();
+        //creates temporary polynomial to hold addition results
+        //worst case: addition creates polynomial with no like terms
+        int maxPolyLen = this.poly.length + addPoly.poly.length;
+        double[] tempPoly = new double[maxPolyLen];
+        int[] tempExpo = new int[maxPolyLen];
+        //keeps track of the size of temporary polynomial
+        int newPolyIndex = 0;
+
+        int i = 0;
+        int j = 0;
+        //addition of polynomials: traverses both polynomials until it reaches the end of one of them
+        //if like term found between two polynomials, add them together and adds into temporary polynomial if coefficient is not zero
+        //if exponent in one term is smaller than the exponent in the other term (no like terms), adds the term with smaller exponent if coefficient is not zero
+        while (i < this.poly.length && j < addPoly.poly.length) {
+            if (this.expo[i] == addPoly.expo[j]) {
+                double newCoeff = this.poly[i] + addPoly.poly[j];
+                if (newCoeff != 0) {
+                    tempPoly[newPolyIndex] = newCoeff;
+                    tempExpo[newPolyIndex] = this.expo[i];
+                    newPolyIndex++;
+                }
+                i++;
+                j++;
+            }
+            else if (this.expo[i] < addPoly.expo[j]) {
+                if (this.poly[i] != 0) {
+                    tempPoly[newPolyIndex] = this.poly[i];
+                    tempExpo[newPolyIndex] = this.expo[i];
+                    newPolyIndex++;
+                }
+                i++;
+            }
+            else {
+                if (addPoly.poly[j] != 0) {
+                    tempPoly[newPolyIndex] = addPoly.poly[j];
+                    tempExpo[newPolyIndex] = addPoly.expo[j];
+                    newPolyIndex++;
+                }
+                j++;
+            }
+        }
+        //adds remainder terms into temporary polynomial
+        while (i < this.poly.length) {
+            if (this.poly[i] != 0) {
+                tempPoly[newPolyIndex] = this.poly[i];
+                tempExpo[newPolyIndex] = this.expo[i];
+                newPolyIndex++;
+            }
+            i++;
+        }
+        while (j < addPoly.poly.length) {
+            if (addPoly.poly[j] != 0) {
+                tempPoly[newPolyIndex] = addPoly.poly[j];
+                tempExpo[newPolyIndex] = addPoly.expo[j];
+                newPolyIndex++;
+            }
+            j++;
+        }
+        //creates result polynomial with actual size of temporary polynomial
+        double[] newPoly = new double[newPolyIndex];
+        int[] newExpo = new int[newPolyIndex];
+        //parses the values of temporary polynomial into result polynomial
+        for (int n = 0; n < newPolyIndex; n++) {
+            newPoly[n] = tempPoly[n];
+            newExpo[n] = tempExpo[n];
+        }
+        return new Polynomial(newPoly, newExpo);
+    }
+    //evaluate polynomial - returns the result of polynomial according to argument value x and corresponding exponent
     public double evaluate(double x) {
         double result = 0;
         for (int i = 0; i < poly.length; i++) {
-            result += poly[i] * Math.pow(x, i);
+            result += poly[i] * Math.pow(x, expo[i]);
         }
         return result;
     }
     //check root - returns true/false if argument value x is a root of the polynomial (equals 0 when evaluated with x)
     public boolean hasRoot(double x) {
         return evaluate(x) == 0;
+    }
+    //multiply two polynomials - multiplies polynomials into one polynomial, then combine like terms and return new polynomial
+    public Polynomial multiply(Polynomial multiplyPoly) {
+        //creates temporary polynomial to hold multiplication results
+        //worst case: multiplication creates polynomial with no like terms
+        int maxPolyLen = this.poly.length * multiplyPoly.poly.length;
+        double[] tempPoly = new double[maxPolyLen];
+        int[] tempExpo = new int[maxPolyLen];
+        //keeps track of the size of temporary polynomial
+        int tempPolyIndex = 0;
+        //multiplication of polynomial and adds into temporary polynomial if coefficient is not zero
+        for (int i = 0; i < this.poly.length; i++) {
+            for (int j = 0; j < multiplyPoly.poly.length; j++) {
+                double newCoeff = this.poly[i] * multiplyPoly.poly[j];
+                int newExpo = this.expo[i] + multiplyPoly.expo[j];
+
+                if (newCoeff != 0) {
+                    tempPoly[tempPolyIndex] = newCoeff;
+                    tempExpo[tempPolyIndex] = newExpo;
+                    tempPolyIndex++;
+                }
+            }
+        }
+        //creates another temporary polynomial to hold like terms combination results
+        //worst case: same size as previous temporary polynomial
+        double[] combPoly = new double[tempPolyIndex];
+        int[] combExpo = new int[tempPolyIndex];
+        //keeps track of the size of temporary polynomial
+        int combPolyIndex = 0;
+        //combination of like terms - iterates through itself to find and combine like terms, adds into result polynomial if coefficient is not zero
+        for (int i = 0; i < tempPolyIndex; i++) {
+            //double totalPoly = tempPoly[i];
+            for (int j = i+1; j < tempPolyIndex; j++) {
+                //if like term found, combines into original term, resets like term to 0 after combination
+                if (tempExpo[i] == tempExpo[j]) {
+                    tempPoly[i] += tempPoly[j];
+                    tempPoly[j] = 0;
+                }
+            }
+            if (tempPoly[i] != 0) {
+                combPoly[combPolyIndex] = tempPoly[i];
+                combExpo[combPolyIndex] = tempExpo[i];
+                combPolyIndex++;
+            }
+        }
+        //creates result polynomial with actual size of temporary polynomial
+        double[] newPoly = new double[combPolyIndex];
+        int[] newExpo = new int[combPolyIndex];
+        //parses the values of temporary polynomial into result polynomial
+        for (int n = 0; n < combPolyIndex; n++) {
+            newPoly[n] = combPoly[n];
+            newExpo[n] = combExpo[n];
+        }
+        return new Polynomial(newPoly, newExpo);
+    }
+    //save to a file - writes polynomial according to polynomial and exponent terms into a file
+    public void saveToFile(String fileName) throws IOException {
+        FileWriter output = new FileWriter(fileName);
+        String outPoly = "";
+        //iterates through polynomial, concatenate symbols and values into string accordingly for output
+        for (int i = 0; i < poly.length; i++) {
+            if (poly[i] != 0) {
+                if (outPoly.length() > 0 && poly[i] > 0) {
+                    outPoly = outPoly.concat("+");
+                }
+
+                if (poly[i] == -1 && expo[i] != 0) {
+                    outPoly = outPoly.concat("-");
+                }
+                else if (poly[i] != 1 || expo[i] == 0) {
+                    outPoly = outPoly.concat(Double.toString(poly[i]));
+                }
+
+                if (expo[i] != 0) {
+                    outPoly = outPoly.concat("x");
+                    if (expo[i] != 1) {
+                        outPoly = outPoly.concat(Integer.toString(expo[i]));
+                    }
+                }
+            }
+        }
+        //writes string into output file and saves the file
+        output.write(outPoly);
+        output.close();
     }
 }
